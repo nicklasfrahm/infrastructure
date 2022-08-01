da1f5cbc393b8a95ad01373fdb0cec066047de8f
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-08-01 23:22:56 +0200

    fix(terraform): use cloud init to install runner

--- deploy/terraform/modules/gce_github_actions_runner/main.tf
+++ deploy/terraform/modules/gce_github_actions_runner/main.tf
@@ -16,6 +16,8 @@ resource "google_compute_instance" "runner" {
   name         = var.vm.hostname
   machine_type = "e2-micro"
 
+  # desc
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
@@ -41,34 +43,36 @@ resource "google_compute_instance" "runner" {
 hostname: ${var.vm.hostname}
 fqdn: ${var.vm.hostname}.${var.vm.fqdn}
 prefer_fqdn_over_hostname: true
+
+runcmd:
+  - |
+    #!/bin/bash
+
+    # Create a folder.
+    mkdir /actions-runner && cd /actions-runner
+
+    # Download the latest runner package.
+    curl -o actions-runner-linux-x64-${var.runner.version}.tar.gz -L https://github.com/actions/runner/releases/download/v${var.runner.version}/actions-runner-linux-x64-${var.runner.version}.tar.gz
+
+    # Extract the installer.
+    tar xzf ./actions-runner-linux-x64-${var.runner.version}.tar.gz
+
+    # Configure the runner and start the configuration experience.
+    RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/${var.github.username}/${var.github.repository} --token ${var.runner.token} --unattended --ephemeral
+
+    # Install the runner as a systemd service.
+    RUNNER_ALLOW_RUNASROOT=1 ./svc.sh install
+
+    # Start the runner.
+    RUNNER_ALLOW_RUNASROOT=1 ./svc.sh start
+
+    # Check status of the runner.
+    RUNNER_ALLOW_RUNASROOT=1 ./svc.sh start
 EOT
   }
 
   # Using this ensures that a new VM will be provisioned if the version of the runner changes.
-  metadata_startup_script = <<EOT
-#!/bin/bash
-
-# Create a folder.
-mkdir /actions-runner && cd /actions-runner
-
-# Download the latest runner package.
-curl -o actions-runner-linux-x64-${var.runner.version}.tar.gz -L https://github.com/actions/runner/releases/download/v${var.runner.version}/actions-runner-linux-x64-${var.runner.version}.tar.gz
-
-# Extract the installer.
-tar xzf ./actions-runner-linux-x64-${var.runner.version}.tar.gz
-
-# Configure the runner and start the configuration experience.
-RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/${var.github.username}/${var.github.repository} --token ${var.runner.token} --unattended --ephemeral
-
-# Install the runner as a systemd service.
-RUNNER_ALLOW_RUNASROOT=1 ./svc.sh install
-
-# Start the runner.
-RUNNER_ALLOW_RUNASROOT=1 ./svc.sh start
-
-# Check status of the runner.
-RUNNER_ALLOW_RUNASROOT=1 ./svc.sh start
-EOT
+  metadata_startup_script = "echo ${var.runner.version} > /dev/null"
 
   depends_on = [google_compute_address.runner]
 }
