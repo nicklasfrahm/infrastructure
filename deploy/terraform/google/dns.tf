784fe95d7a67ba16e1ff11b362ad0f6f06fde6a9
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-08-23 19:49:45 +0200

    fix(netlifycms): use specific subdomain

--- deploy/terraform/google/dns.tf
+++ deploy/terraform/google/dns.tf
@@ -47,7 +47,7 @@ module "google_dns_site_deflf02" {
 
 resource "google_dns_record_set" "dev_nicklasfrahm_api" {
   managed_zone = module.google_dns_zone_nicklasfrahm_dev.name
-  name         = "api.nicklasfrahm.dev."
+  name         = "netlifycms.nicklasfrahm.dev."
   type         = "CNAME"
   ttl          = 600
 
