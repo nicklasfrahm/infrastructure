f8cea53df29d53d231e23c5d7504a432276c3b8c
Nicklas Frahm <nicklas.frahm@gmail.com>
2023-03-29 22:53:31 +0200

    feat(dns): allow creation of DNS for physical location

--- pkg/pulumi/dns/types.go
+++ pkg/pulumi/dns/types.go
@@ -3,6 +3,8 @@ package dns
 const (
 	// RecordTypeGithubPages is the type of a GitHub Pages DNS record.
 	RecordTypeGithubPages = "GITHUB_PAGES"
+	// RecordTypeSite is the type of a DNS record for a site.
+	RecordTypeSite = "SITE"
 )
 
 // GitHubPagesRecordSpec is a data structure that describes a GitHub Pages DNS record.
@@ -11,16 +13,24 @@ type GitHubPagesRecordSpec struct {
 	Org string `yaml:"org"`
 }
 
+// SiteRecordSpec is a data structure that describes a DNS record for a site.
+type SiteRecordSpec struct {
+	// Router is the FQDN of the edge router.
+	Router string `yaml:"router"`
+}
+
 // RecordSpec is a data structure that describes a DNS record.
 type RecordSpec struct {
 	// Name is the name of the DNS record.
 	Name string `yaml:"name" validate:"required"`
 	// Type is the type of the DNS record.
-	Type string `yaml:"type" validate:"required,oneof=GITHUB_PAGES"`
+	Type string `yaml:"type" validate:"required,oneof=GITHUB_PAGES SITE"`
 	// Values is a list of values for the DNS record.
 	Values []string `yaml:"values"`
 	// GithubPages is configures the GitHub pages site.
 	GithubPages GitHubPagesRecordSpec `yaml:"githubPages"`
+	// Site configures a site DNS record.
+	Site SiteRecordSpec `yaml:"site"`
 }
 
 // ZoneSpec is a data structure that describes a DNS zone.
