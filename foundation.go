09a55bb13d5f670c90188bcbacd0efeb9660151c
Nicklas Frahm <nicklas.frahm@gmail.com>
2023-11-08 00:31:01 +0100

    feat(pulumi): create first prototype for k3se component

--- foundation.go
+++ foundation.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"gopkg.in/yaml.v3"
 
 	"github.com/nicklasfrahm/infrastructure/pkg/pulumi/dns"
+	"github.com/nicklasfrahm/infrastructure/pkg/pulumi/kubernetes"
 )
 
 const (
@@ -23,7 +25,12 @@ const (
 // NewFoundationStack deploys DNS, Kubernetes at the network edge
 // and underlay networking.
 func NewFoundationStack(ctx *pulumi.Context) error {
-	if err := configureDNS(ctx); err != nil {
+	zoneResources, err := configureDNS(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := configureClusters(ctx, pulumi.DependsOn(zoneResources)); err != nil {
 		return err
 	}
 
@@ -31,28 +38,63 @@ func NewFoundationStack(ctx *pulumi.Context) error {
 }
 
 // configureDNS loads the DNS specification and configures DNS.
-func configureDNS(ctx *pulumi.Context) error {
+func configureDNS(ctx *pulumi.Context, opts ...pulumi.ResourceOption) ([]pulumi.Resource, error) {
 	dnsSpecBytes, err := os.ReadFile(dnsSpecPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var dnsSpec dns.Spec
 	if err := yaml.Unmarshal(dnsSpecBytes, &dnsSpec); err != nil {
-		return err
+		return nil, err
 	}
 
 	if err := validator.New().Struct(dnsSpec); err != nil {
-		return err
+		return nil, err
 	}
 
+	resources := make([]pulumi.Resource, len(dnsSpec.Zones))
 	for i := 0; i < len(dnsSpec.Zones); i++ {
 		zone := &dnsSpec.Zones[i]
 
-		_, err := dns.NewZone(ctx, fmt.Sprintf("%s-c.zone-%s", StackFoundation, zone.Name), zone)
+		resource, err := dns.NewZone(ctx, fmt.Sprintf("%s-c.zone-%s", StackFoundation, zone.Name), zone, opts...)
+		if err != nil {
+			return nil, err
+		}
+
+		resources[i] = resource
+	}
+
+	return resources, nil
+}
+
+// configureEdgeClusters creates k3s clusters at the network edge.
+func configureClusters(ctx *pulumi.Context, opts ...pulumi.ResourceOption) error {
+	entries, err := os.ReadDir(kubernetes.K3seSpecDir)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		clusterName := strings.TrimSuffix(entry.Name(), ".yaml")
+		cluster, err := kubernetes.NewK3se(ctx, fmt.Sprintf("%s-c.k3se-%s", StackFoundation, clusterName), &kubernetes.K3seArgs{
+			Name: clusterName,
+		}, opts...)
 		if err != nil {
 			return err
 		}
+
+		// TODO: Remove this.
+		cluster.Server.ApplyT(func(server string) error {
+			if server != "" {
+				pulumi.Printf("%s: %s\n", clusterName, server)
+			}
+			return nil
+		})
 	}
 
 	return nil
--- pkg/pulumi/kubernetes/k3se.go
+++ pkg/pulumi/kubernetes/k3se.go
@@ -1,18 +1,31 @@
-package dns
+package kubernetes
 
 import (
+	"fmt"
+	"path"
+
 	"github.com/go-playground/validator/v10"
+	"github.com/pulumi/pulumi-command/sdk/go/command/local"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
 	// ComponentTypeK3se is the ID of the component type.
 	ComponentTypeK3se = "nicklasfrahm:kubernetes:K3se"
+	// K3seSpecDir is the directory where k3se configurations are stored.
+	K3seSpecDir = "deploy/k3se"
+
+	// kubeConfigDir is the path where the kubeconfig files will be stored.
+	kubeConfigDir = "./"
 )
 
 // K3se manages a k3s cluster using k3se.
 type K3se struct {
 	pulumi.ResourceState
+
+	// Server is the address of the Kubernetes API server.
+	Server pulumi.StringOutput `pulumi:"server"`
 }
 
 // K3seArgs are the arguments for creating a k3s cluster using k3se.
@@ -31,10 +44,46 @@ func NewK3se(ctx *pulumi.Context, name string, args *K3seArgs, opts ...pulumi.Re
 	if err := validator.New().Struct(args); err != nil {
 		return nil, err
 	}
+	outputs := pulumi.Map{}
 
-	// TODO: Implement logic here.
+	// TODO: Remove this once we verified that this won't break anything.
+	if args.Name == "charlie" {
+		specPath := path.Join(K3seSpecDir, fmt.Sprintf("%s.yaml", args.Name))
+		kubeConfigPath := path.Join(kubeConfigDir, fmt.Sprintf("%s.kubeconfig.yaml", args.Name))
+		cmd, err := local.NewCommand(ctx, fmt.Sprintf("%s-r.command-k3se", name), &local.CommandArgs{
+			Create: pulumi.Sprintf("k3se up %s -k %s", specPath, kubeConfigPath),
+			Delete: pulumi.Sprintf("k3se down %s", specPath, kubeConfigPath),
+			AssetPaths: pulumi.StringArray{
+				pulumi.String(kubeConfigPath),
+			},
+		}, pulumi.Parent(component))
+		if err != nil {
+			return nil, err
+		}
 
-	if err := ctx.RegisterResourceOutputs(component, pulumi.Map{}); err != nil {
+		component.Server = cmd.Assets.ApplyT(func(item map[string]pulumi.AssetOrArchive) string {
+			// Ignore empty assets during the preview.
+			if item == nil {
+				return ""
+			}
+
+			asset, ok := item[kubeConfigPath].(pulumi.Asset)
+			if !ok {
+				return fmt.Errorf("expected pulumi.Asset: received %T", item).Error()
+			}
+
+			config, err := clientcmd.LoadFromFile(asset.Path())
+			if err != nil {
+				return err.Error()
+			}
+
+			return config.Clusters[config.CurrentContext].Server
+		}).(pulumi.StringOutput)
+
+		outputs["server"] = component.Server
+	}
+
+	if err := ctx.RegisterResourceOutputs(component, outputs); err != nil {
 		return nil, err
 	}
 
