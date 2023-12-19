df14beec0df70c1d66a07a4c2662925e3f5c207f
Nicklas Frahm <nicklas.frahm@gmail.com>
2023-12-20 00:54:25 +0100

    fix(cli): move router configuration into own type

--- cmd/ic/zone/types.go
+++ cmd/ic/zone/types.go
@@ -13,18 +13,24 @@ var (
 	ErrInvalidHostname = fmt.Errorf("hostname must be lowercase, alphanumeric, and cannot start or end with a hyphen")
 )
 
+// ZoneRouter represents the configuration of a router serving an availability zone.
+type ZoneRouter struct {
+	// Hostname is the hostname of the router.
+	Hostname string `json:"hostname"`
+	// ID is the IPv4 address of the router.
+	ID net.IP `json:"routerID"`
+	// ASN is the autonomous system number of the router.
+	ASN int `json:"asn"`
+}
+
 // Zone represents an availability zone.
 type Zone struct {
 	// Name is the name of the zone.
 	Name string `json:"name"`
-	// Hostname is the hostname of the router serving the zone.
-	Hostname string `json:"hostname"`
-	// RouterID is the IPv4 address of the router serving the zone.
-	RouterID net.IP `json:"routerID"`
-	// ASN is the autonomous system number of the zone.
-	ASN int `json:"asn"`
 	// Domain is the domain that will contain the DNS records for the zone.
-	Domain string `json:"comain"`
+	Domain string `json:"domain"`
+	// Router is the router serving the zone.
+	Router ZoneRouter `json:"router"`
 }
 
 // Validate returns an error if the zone is not valid.
@@ -32,7 +38,7 @@ func (z *Zone) Validate() error {
 	// Ensure that the hostname contains only alphanumeric characters and hyphens,
 	// and that it does not start or end with a hyphen.
 	hostnameRegex := regexp.MustCompile((`^[a-z0-9]+([a-z0-9-]*[a-z0-9]+)*$`))
-	if !hostnameRegex.MatchString(z.Hostname) {
+	if !hostnameRegex.MatchString(z.Router.Hostname) {
 		return ErrInvalidHostname
 	}
 
